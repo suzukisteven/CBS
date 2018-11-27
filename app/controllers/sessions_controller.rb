2c644d4717bd2f63b9cdd031617735e71723d6b0
Steven <suzukisteven@gmail.com>
2018-11-27 18:44:04 +0800

    Text Tool completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,21 +2,21 @@ class SessionsController < Clearance::SessionsController
     def create_from_omniauth
       auth_hash = request.env["omniauth.auth"]
       authentication = Authentication.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"]) ||  Authentication.create_with_omniauth(auth_hash)
-    
+
       # if: previously already logged in with OAuth
       if authentication.user
         user = authentication.user
         authentication.update_token(auth_hash)
         @next = root_url
-        @notice = "Signed in!"
+        @notice = "Successfuly signed in."
       # else: user logs in with OAuth for the first time
       else
         user = User.create_with_auth_and_hash(authentication, auth_hash)
         @next = '/subscribe_to_plan'
         @notice = "User created. Edit details on your profile page."
       end
-    
+
       sign_in(user)
-      redirect_to @next, :notice => @notice
+      redirect_to @next, :success => @notice
     end
 end
--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -1,56 +1,56 @@
 class TextController < ApplicationController
     def show
-        
+
         @text = Database.new
         @texts = Database.all
-        tone_analyzer = ToneAnalyzerV3.new(
-          version: "2017-09-21",
-          iam_apikey: ENV['TEXT_KEY'] ,
-          url: "https://gateway.watsonplatform.net/tone-analyzer/api"
-        ) 
-        if(Database.last)
-            if(Database.last.text_result)   
-                Database.last.text_result.each do |key, value| 
-                    if key == "sentences_tone"
-                        @document_tone = value
-                    end 
-                end 
-            else 
-                text=Database.last.text
-                tone = tone_analyzer.tone(
-                tone_input: {text: text},
-                content_type: "application/json"
-                )
-                @sentence_tone_score= Database.last.text_result
-                @document_tone = []
-                Database.last.update(text_result:tone.result)
-                 Database.last.text_result.each do |key, value| 
-                    if key == "sentences_tone"
-                        @document_tone = value
-                    end 
-                end
 
-             end
-        end
-        respond_to do |format|
-            format.html { render :show }
-        end
     end
 
     def create
-        @text = Database.new(text_params) 
+        @text = Database.new(text_params)
         @text.user_id = current_user.id
-        @text.save  
-        redirect_to text_analyze_path  
+        @text.save
+        tone_analyzer = ToneAnalyzerV3.new(
+          version: "2017-09-21",
+          iam_apikey: ENV['TEXT_KEY'],
+          url: "https://gateway.watsonplatform.net/tone-analyzer/api"
+        )
+        if(Database.last)
+          if(Database.last.text_result)
+
+              Database.last.text_result.each do |key, value|
+                  if key == "sentences_tone"
+                      @document_tone = value
+                  end
+              end
+          else
+              text = Database.last.text
+              tone = tone_analyzer.tone(
+              tone_input: {text: text},
+              content_type: "application/json"
+              )
+              @sentence_tone_score= Database.last.text_result
+              @document_tone = []
+              Database.last.update(text_result:tone.result)
+           end
+        end
+
+        @database = Database.last.to_json
+        p @database
+        
+        respond_to do |format|
+          format.html { redirect_to text_analyze_path }
+          format.js
+        end
     end
-    
+
 
       private
         # Use callbacks to share common setup or constraints between actions.
         def set_text
           @text = Database.find(params[:id])
         end
-    
+
         # Never trust parameters from the scary internet, only allow the white list through.
         def text_params
           params.require(:database).permit(:text)
